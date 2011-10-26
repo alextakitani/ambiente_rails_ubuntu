82b7148a0d9facae2b3e8fda43c71e748d9f6226
Alex <aftakitani@gmail.com>
2011-10-26 14:42:44 -0200

    primeiro up

---
+++ ambiente.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+function usage {
+  echo "Uso: $0 -n seu_nome -e seu_email"
+}
+
+#processa parametros
+if [ $# -eq 0 ] ; then
+	usage
+	exit 1
+fi
+while [ $# -gt 1 ] ; do
+	case $1 in
+		-n) nome=$2 ; shift 2 ;;
+		-e) email=$2 ; shift 2 ;;
+		-*) usage
+			exit 1 ;;
+		*) shift 1 ;;
+	esac
+done
+
+	
+if [ "$nome" = "" ]; then
+	echo 'Erro: nome é obrigatorio'
+	usage
+	exit 1
+fi
+
+if [ "$email" = "" ]; then
+	echo 'Erro: email é obrigatorio'
+	usage
+	exit 1
+fi
+
+#instala as dependencias
+sudo aptitude install ruby1.8 ruby1.8-dev
+sudo aptitude install build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev  g++ openjdk-6-jre-headless subversion libmysqlclient-dev libcurl4-openssl-dev libmagick9-dev libpq-dev
+
+#instala o rvm
+if ! [ -s "$HOME/.rvm/scripts/rvm" ] ; then
+curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer -o rvm-installer ; chmod +x rvm-installer ; ./rvm-installer --version latest
+source ~/.bashrc
+rm rvm-installer
+fi
+
+#instala rvm ou o ruby
+if [ -s "$HOME/.rvm/scripts/rvm" ] ; then
+	rvm install 1.9.3	
+	rvm use 1.9.3 --default
+else
+	echo 'Erro: Não consegui encontrar o rvm.'
+	exit 1
+fi
+
+#cria o gemrc e configura para não instalar ri e rdoc
+echo 'gem: --no-ri --no-rdoc' > ~/.gemrc
+
+#configura usuario e senha do git
+git config --global user.name $nome
+git config --global user.email $email
+
+#instala o vim e as dependencias
+sudo aptitude install vim-gtk exuberant-ctags ack-grep
+sudo ln -s /usr/bin/ack-grep /usr/local/bin/ack
+
+rvm use system
+
+git clone https://github.com/krisleech/vimfiles ~/.vim
+cd ~/.vim
+rake install
+
+ln -s ~/.vim/vimrc ~/.vimrc
+
+cd ~/.vim/bundle/command-t/ruby/command-t/
+make clean
+ruby extconf.rb
+make
+cd ~
+
+rvm use 1.9.3
+
+gem install bundler
